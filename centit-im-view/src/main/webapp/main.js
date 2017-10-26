19c037833826957c1ca6c1026721a32a4ac25a3f
lu_hb <lu_hb@centit.com>
2017-10-26 17:30:45 +0800

    1、开始引入requirejs

--- centit-im-view/src/main/webapp/main.js
+++ centit-im-view/src/main/webapp/main.js
@@ -1,12 +1,27 @@
 require.config({
     "baseUrl":"plugins/",
+    "shim":{
+        "jquery.raty":["jquery"],
+        "promise":{},
+        "url":{},
+        "fetch":{},
+        "layui":{},
+        "im-ie":{
+            deps: ['jquery','jquery.raty','promise','url','fetch','layui','mustache','base64']
+        }
+    },
     "paths":{
         "mustache":"mustache.min.js",
         "jquery":"jquery-1.9.1.min.js",
         "base64":"base64.js",
-    },
-    "shim":{
-        "jquery.raty":["jquery"],
-        "shim/promise"
+        "promise":"shim/promise",
+        "url":"url",
+        "fetch":"shim/fetch",
+        "layui":"../src/layui",
+        "im-ie":"../im-ie"
     }
 });
+
+define(["mustache","jquery","layui","base64","promise","url","fetch"],function (mustache,$,layui,base64) {
+    
+})
