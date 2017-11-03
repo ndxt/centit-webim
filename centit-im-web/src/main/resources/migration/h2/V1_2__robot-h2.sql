f3e765c3a68eab4d0c33b3ac04f2a2d91bcde48a
pk <nuist1234>
2017-11-03 14:01:49 +0800

    webim h2数据库初始化

---
+++ centit-im-web/src/main/resources/migration/h2/V1_2__robot-h2.sql
@@ -0,0 +1,15 @@
+create table question_and_answer
+(
+  question_id VARCHAR(48) not null primary key comment 'uuid',
+  os_id VARCHAR(32),
+  opt_Id VARCHAR(32),
+  question_id VARCHAR(100) not null primary key comment 'uuid',
+  key_words VARCHAR(1000) comment '主题词 多个主题词用空格分开',
+  question_title VARCHAR(1000) comment '问题',
+  question_url   VARCHAR(1000) comment '问题',
+  question_answer      TEXT comment '回答',
+  creator     VARCHAR(100) comment '创建人',
+  create_time  DATE comment '创建时间',
+  last_update_time  DATE comment '更新时间',
+  delete_sign   CHAR(1) comment '是否删除 T/F'
+);
