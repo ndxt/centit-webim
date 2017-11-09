1397d1d9e049d9fcc937fedc57112a8dc7fa6309
lu_sn <lu_sn@centit.com>
2017-11-09 09:52:24 +0800

    手机页面模块化

---
+++ centit-im-view/src/main/webapp/src/mobile.js
@@ -0,0 +1,27 @@
+/**
+ * Created by lu_sn on 2017/11/8.
+ */
+require(["../plugins/jquery-1.9.1.min", "../plugins/base64", "../src/js/ie/Service.class"], function ($,base64,ServiceIM) {
+    var result = parseURL(),
+        token = result.params.token,
+        params = JSON.parse(decodeURIComponent(base64.decode(token)));
+    mine = {
+        userCode: params.userCode,
+        userName: params.userName,
+        switchServiceBtn:params.switchServiceBtn || ""
+    },
+
+        document.title = 'KF - ' + params.userName
+
+    layui.use(['mobile', 'layer'], function (im, layer) {
+       var mobile = layui.mobile
+        ,im = mobile.layim;
+        new ServiceIM(im, layui.$.extend({
+            userType: 'S'
+        }, mine), {layer: layer})
+    })
+})
+
+// define(['Params', 'ServiceIM'], function(Params, ServiceIM) {
+//     new ServiceIM(Params.parse())
+// })
