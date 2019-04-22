c888405d47a7794ca14eff77bf51123d681ad673
杨淮生 <codefan@centit.com>
2019-04-22 14:19:33 +0800

    修改 版本 并且将 service 修改为 webim

---
+++ centit-im-module/src/main/java/com/centit/im/controller/WebImMsgController.java
@@ -0,0 +1,40 @@
+package com.centit.im.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
+import com.centit.framework.common.JsonResultUtils;
+import com.centit.framework.common.ResponseMapData;
+import com.centit.framework.core.controller.BaseController;
+import com.centit.framework.model.basedata.IUnitInfo;
+import com.centit.im.po.WebImCustomer;
+import com.centit.im.po.WebImFriendMemo;
+import com.centit.im.po.WebImGroup;
+import com.centit.im.po.WebImGroupMember;
+import com.centit.im.service.WebImMessageManager;
+import com.centit.im.service.WebImUserManager;
+import com.centit.support.algorithm.CollectionsOpt;
+import com.centit.support.algorithm.StringBaseOpt;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.*;
+
+/**
+ * Created by codefan on 17-5-26.
+ */
+@Controller
+@RequestMapping("/webimmsg")
+public class WebImMsgController extends BaseController {
+
+    @Resource
+    protected WebImMessageManager webImMessageManager;
+
+}
