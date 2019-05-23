8cd3288ef644671c5de4421d46d5a6d7f74e999a
zhf <nju1@digiwin.biz>
2019-05-23 13:05:41 +0800

    ESsearch,ESindex加入bean

--- centit-im-robot-es/src/main/java/com/centit/im/robot/es/controller/QuestAndAnswerController.java
+++ centit-im-robot-es/src/main/java/com/centit/im/robot/es/controller/QuestAndAnswerController.java
@@ -1,22 +1,27 @@
 package com.centit.im.robot.es.controller;
 
 import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.centit.framework.common.JsonResultUtils;
 import com.centit.framework.common.ResponseData;
 import com.centit.framework.common.ResponseMapData;
 import com.centit.framework.core.controller.BaseController;
 import com.centit.framework.core.controller.WrapUpResponseBody;
+import com.centit.framework.core.dao.PageQueryResult;
+import com.centit.search.service.Impl.ESIndexer;
+import com.centit.search.service.Impl.ESSearcher;
+import com.centit.support.algorithm.NumberBaseOpt;
+import com.centit.support.algorithm.StringBaseOpt;
 import com.centit.support.database.utils.PageDesc;
 import com.centit.im.robot.es.po.QuestAndAnswer;
 import com.centit.im.robot.es.service.QuestAndAnswerManager;
 import com.centit.support.algorithm.DatetimeOpt;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.apache.commons.lang3.tuple.Pair;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
@@ -35,14 +40,17 @@ public class QuestAndAnswerController extends BaseController {
 
     @Resource
     protected QuestAndAnswerManager questAndAnswerManager;
+    @Autowired(required = false)
+    private ESSearcher esObjectSearcher;
 
-    /** 条件查询
-     * @param request  {@link HttpServletRequest}
+    /**
+     * 条件查询
      *
+     * @param request {@link HttpServletRequest}
      * @return {data:[]}
      */
     @ApiOperation(value = "1条件查询")
-    @RequestMapping(value = "/listAll",method = RequestMethod.GET)
+    @RequestMapping(value = "/listAll", method = RequestMethod.GET)
     @WrapUpResponseBody
     public JSONArray list(PageDesc pageDesc, HttpServletRequest request) {
         Map<String, Object> searchColumn = convertSearchColumn(request);
@@ -50,9 +58,10 @@ public class QuestAndAnswerController extends BaseController {
     }
 
 
-    /** 查询详情
-     * @param questionId  questionId
+    /**
+     * 查询详情
      *
+     * @param questionId questionId
      * @return {data:{}}
      */
     @ApiOperation(value = "2查询详情")
@@ -64,20 +73,23 @@ public class QuestAndAnswerController extends BaseController {
 
     /**
      * 新增
+     *
      * @return
      */
     @ApiOperation(value = "3新增")
-    @RequestMapping(value="/addQuestion",method = {RequestMethod.POST})
+    @RequestMapping(value = "/addQuestion", method = {RequestMethod.POST})
     @WrapUpResponseBody
     public ResponseData createQuestionCatalog(@RequestBody QuestAndAnswer questAndAnswer) throws IOException {
         questAndAnswer.setCreateTime(DatetimeOpt.currentUtilDate());
         questAndAnswerManager.saveNewObject(questAndAnswer);
+
         return ResponseData.makeSuccessResponse();
     }
 
     /**
      * 删除
-     * @param questionId  questionId
+     *
+     * @param questionId questionId
      */
     @ApiOperation(value = "4删除")
     @RequestMapping(value = "/deleteQuestion/{questionId}", method = {RequestMethod.DELETE})
@@ -89,7 +101,8 @@ public class QuestAndAnswerController extends BaseController {
 
     /**
      * 标记删除
-     * @param questionId  questionId
+     *
+     * @param questionId questionId
      */
     @ApiOperation(value = "5标记删除")
     @RequestMapping(value = "/delete/{questionId}", method = {RequestMethod.DELETE})
@@ -100,24 +113,28 @@ public class QuestAndAnswerController extends BaseController {
     }
 
 
-    /** 修改
-     * @param response    {@link HttpServletResponse}
+    /**
+     * 修改
+     *
+     * @param response {@link HttpServletResponse}
      */
     @ApiOperation(value = "6修改")
     @RequestMapping(value = "/updateQuestion", method = {RequestMethod.PUT})
     @WrapUpResponseBody
     public ResponseData updateQuestionCatalog(
-                                      @RequestBody QuestAndAnswer questAndAnswer,
-                                      HttpServletResponse response) throws IOException {
-
-        questAndAnswerManager.updateQuestionCatalog( questAndAnswer);
+            @RequestBody QuestAndAnswer questAndAnswer,
+            HttpServletResponse response) throws IOException {
+        questAndAnswerManager.updateQuestionCatalog(questAndAnswer);
         return ResponseData.makeSuccessResponse();
     }
+
     @ApiOperation(value = "7es查询")
-    @RequestMapping(value = "/listES/{question}",method = RequestMethod.GET)
+    @RequestMapping(value = "/listES/{question}", method = RequestMethod.GET)
     @WrapUpResponseBody
-    public JSONArray listEs(String squestion,PageDesc pageDesc) {
-        return questAndAnswerManager.listESsAsJson(squestion,pageDesc);
+    public PageQueryResult<Map<String, Object>> listEs(String question, PageDesc pageDesc) {
+        Pair<Long, List<Map<String, Object>>> res = esObjectSearcher.search(question, pageDesc.getPageNo(), pageDesc.getPageSize());
+        pageDesc.setTotalRows(NumberBaseOpt.castObjectToInteger(res.getLeft()));
+        return PageQueryResult.createResult(res.getRight(), pageDesc);
     }
 
 }
